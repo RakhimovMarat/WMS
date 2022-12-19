541cb3b829b1fb961dc1be408da49c56d065913a
marat <mf.rakhimov1982@gmail.com>
2022-12-19 22:51:43 +0300

    Create migration for Item

---
+++ db/migrate/20221219162624_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :number
+      t.string :description
+
+      t.timestamps
+    end
+    add_index :items, :number
+  end
+end
