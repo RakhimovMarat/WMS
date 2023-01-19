2c5276f3f97311dbe19a330c6d29b118f6a3eeb7
marat <mf.rakhimov1982@gmail.com>
2023-01-19 21:04:38 +0300

    Create spec for address model

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe Address do
+	describe "validations" do
+		it { should validate_presence_of :cell }
+		it { should validate_uniqueness_of :cell }
+	end
+
+	describe "assotiations" do
+		it { should have_one :item }
+	end
+
+end
