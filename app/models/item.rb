16886137fd232ff321afc7c27bad6f14537b1af0
marat <mf.rakhimov1982@gmail.com>
2023-02-10 15:21:37 +0300

    Update item and stock models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
   validates :number, :description, presence: true
   validates :number, uniqueness: true
   validates :address_id, uniqueness: true
 
   belongs_to :address, optional: true
-  has_many :stocks
+  has_many :stocks, dependent: :destroy
 
   def self.search(search)
     if search
-      where(["number LIKE ?","%#{search}%"])
+      where(['number LIKE ?', "%#{search}%"])
     else
       Item.all.order(:number)
     end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Stock < ApplicationRecord
   validates :quantity, :flow, presence: true
 
   belongs_to :item, optional: true
 
-  enum flow: { receipt: 1, expense: -1 }
+  enum flow: { receipt: 1, expense: -1 }     # enumeration to in/out transactions
 end
