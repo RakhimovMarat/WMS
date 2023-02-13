62a39c1d33ff71413105f4868b927f86b715faad
marat <mf.rakhimov1982@gmail.com>
2023-02-13 18:32:04 +0300

    Update item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 class ItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_item, only: %i[show edit update destroy]
 
   def index
-	  @item = Item.search(params[:search])
-    @item = @item.includes(:address)     #adding eager load
+    @item = Item.search(params[:search])
+    @item = @item.includes(:address)     # adding eager load
+    @stock = Stock.group(:item_id).sum('flow * quantity')
   end
 
   def new
@@ -14,10 +17,10 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      flash[:success] = "Part was saved"
+      flash[:success] = 'Part was saved'
       redirect_to @item
     else
-      flash.now[:error] = "Please fill all fields"
+      flash.now[:error] = 'Please fill all fields'
       render action: 'new'
     end
   end
@@ -30,19 +33,19 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      flash[:success] = "Part was updated"
+      flash[:success] = 'Part was updated'
       redirect_to @item
     else
-      flash.now[:error] = "Please fill all fields"
+      flash.now[:error] = 'Please fill all fields'
       render action: 'edit'
     end
   end
 
   def destroy
-    if @item.destroy.destroyed?
-      flash[:error] = "Part was deleted"
-      redirect_to item_path
-    end
+    return unless @item.destroy.destroyed?
+
+    flash[:error] = 'Part was deleted'
+    redirect_to item_path
   end
 
   private
@@ -52,6 +55,6 @@ class ItemsController < ApplicationController
   end
 
   def find_item
-  	@item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 end
