3e7c817b1a6cb0ccb23721a4ddc95f6f44c258f3
marat <mf.rakhimov1982@gmail.com>
2023-02-28 21:41:38 +0300

    Update Order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,33 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @order = Order.all
+  end
+
+  def new; end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash[:success] = 'Order was saved'
+      redirect_to @order
+    else
+      flash.now[:error] = 'Please fill all fields'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    return unless @order.destroy.destroyed?
+    flash[:error] = 'Order was deleted'
+    redirect_to order_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity, :item_id)
+  end
 end
